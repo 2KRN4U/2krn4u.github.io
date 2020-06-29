9937984136275a2556c160b5dd1b7250fd258e34
2KRN4U <2krn4u@gmail.com>
2020-06-28 23:01:35 -0700

    cleanup
    i dont even know what the point of this site is lmao

--- chuchuyeah/main.js
+++ chuchuyeah/main.js
@@ -1,5 +1,11 @@
 // JavaScript Document
-var subOn = true;
+var track = 'opening.ass';
+var curTitle = '青空のラプソディ';
+var curAlbum = '青空のラプソディ';
+var curArt = [{src: 'https://i.imgur.com/DExPG8h.jpg', sizes: '500x500', type: 'image/jpg'}, ];
+var curArtist = 'fhána';
+var videojsPlayer;
+var curVid = 0;
 
 function setup() {
 	videojs('video').ready(function () {
@@ -8,8 +14,8 @@ function setup() {
         window.SubtitlesOctopusOnLoad = function () {
             var options = {
                 video: video,
-                subUrl: 'opening.ass',
-                fonts: ['OpenSans.ttf'],
+                subUrl: track,
+                fonts: ['Advert-Bold.otf', 'Raleigh Bold BT.ttf'],
                 //onReady: onReadyFunction,
                 //debug: true,
                 workerUrl: 'subtitles-octopus-worker.js'
@@ -20,15 +26,92 @@ function setup() {
             SubtitlesOctopusOnLoad();
         }
     });
-	
+    updateMetadata();
+    videojsPlayer = videojs('video');
 };
 
 function toggleSubtitles() {
-	if (subOn) {
-		window.octopusInstance.freeTrack();
-		subOn = false;
+    if (document.getElementById("subactive").value === "None") {
+        octopusInstance.freeTrack();
+
+    } else if (curVid == 0) {
+        if (document.getElementById("subactive").value === "Lyrics") {
+            track = 'opening.ass';
+            octopusInstance.setTrackByUrl(track);
+
+        } 
+
+    } else if (curVid == 1) {
+        if (document.getElementById("subactive").value === "Lyrics") {
+            track = 'ending.ass';
+            octopusInstance.setTrackByUrl(track);
+		}
+    }
+}
+
+navigator.mediaSession.setActionHandler('play', function() {
+    videojsPlayer.play();
+});
+
+navigator.mediaSession.setActionHandler('pause', function() {
+    videojsPlayer.pause();
+});
+
+navigator.mediaSession.setActionHandler('nexttrack', function() {
+    if (curVid == 0){
+		changeVideoED();
 	} else {
-		window.octopusInstance.setTrackByUrl('opening.ass');
-		subOn = true;
+		changeVideoOP();
 	}
+});
+
+navigator.mediaSession.setActionHandler('previoustrack', function() {
+    if (curVid == 0){
+		changeVideoED();
+	} else {
+		changeVideoOP();
+	}
+});
+
+function updateMetadata() {
+    navigator.mediaSession.metadata = new MediaMetadata({
+        title: curTitle,
+        artist: curArtist,
+        album: curAlbum,
+        artwork: curArt
+    });
+}
+
+function changeVideoOP() {
+    if (curVid == 1) {
+        curTitle = '青空のラプソディ';
+		curAlbum = '青空のラプソディ';
+		curArt = [{src: 'https://i.imgur.com/DExPG8h.jpg', sizes: '500x500', type: 'image/jpg'}, ];
+		curArtist = 'fhána';
+		document.title = "Aozora no Rhapsody | 青空のラプソディ";
+        videojsPlayer.src("https://animethemes.moe/video/KobayashiSanChiNoMaidDragon-OP1-NCBD1080.webm");
+		document.getElementById("subactive").innerHTML = "";
+		document.getElementById("subactive").innerHTML = "<option>Lyrics</option><option>None</option>";
+		updateMetadata();
+		curVid = 0;
+		toggleSubtitles();
+		videojsPlayer.play();
+    }
+}
+
+function changeVideoED() {
+    if (curVid == 0) {
+        curTitle = 'イシュカン・コミュニケーション';
+		curAlbum = 'イシュカン・コミュニケーション';
+		curArt = [{src: 'https://i.imgur.com/laTkEnu.jpg', sizes: '500x497', type: 'image/jpg'}, ];
+		curArtist = 'ちょろゴンず';
+		document.title = "Ishukan Communication | イシュカン・コミュニケーション";
+        videojsPlayer.src("https://animethemes.moe/video/KobayashiSanChiNoMaidDragon-ED1-NCBD1080.webm");
+		document.getElementById("subactive").innerHTML = "";
+		document.getElementById("subactive").innerHTML = "<option>Lyrics</option><option>None</option>";
+		updateMetadata();
+		curVid = 1;
+		toggleSubtitles();
+		videojsPlayer.play();
+    }
 }
