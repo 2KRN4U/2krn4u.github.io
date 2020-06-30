63825197de108a81edcf8bbd69b9207bb38ccb1a
2KRN4U <2krn4u@gmail.com>
2020-06-29 22:25:18 -0700

    standardized

--- mikakunin/main.js
+++ mikakunin/main.js
@@ -1,5 +1,10 @@
 // JavaScript Document
-var subOn = true;
+var track = 'opening.ass';
+var curTitle = 'まっしろわーるど';
+var curAlbum = 'まっしろわーるど';
+var curArt = [{src: 'https://i.imgur.com/vkw4Kra.jpg', sizes: '500x494', type: 'image/jpg'}, ];
+var curArtist = 'みかくにんぐッ!';
+var videojsPlayer;
 
 function setup() {
 	videojs('video').ready(function () {
@@ -20,15 +25,33 @@ function setup() {
             SubtitlesOctopusOnLoad();
         }
     });
-	
+	updateMetadata();
+    videojsPlayer = videojs('video');
 };
 
 function toggleSubtitles() {
-	if (subOn) {
-		window.octopusInstance.freeTrack();
-		subOn = false;
-	} else {
-		window.octopusInstance.setTrackByUrl('opening.ass');
-		subOn = true;
-	}
+    if (document.getElementById("subactive").value === "None") {
+        octopusInstance.freeTrack();
+
+    } else if (document.getElementById("subactive").value === "Lyrics") {
+        track = 'opening.ass';
+        octopusInstance.setTrackByUrl(track);
+    }
+}
+
+navigator.mediaSession.setActionHandler('play', function() {
+    videojsPlayer.play();
+});
+
+navigator.mediaSession.setActionHandler('pause', function() {
+    videojsPlayer.pause();
+});
+
+function updateMetadata() {
+    navigator.mediaSession.metadata = new MediaMetadata({
+        title: curTitle,
+        artist: curArtist,
+        album: curAlbum,
+        artwork: curArt
+    });
 }
