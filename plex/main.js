429ee20ed8474cffa83239d6610a2532edff9b5b
2KRN4U <2krn4u@gmail.com>
2020-07-07 09:28:18 -0700

    cleaner look for sort option? may improve later on

--- plex/main.js
+++ plex/main.js
@@ -22,16 +22,18 @@ function sortBy(){
 	if (document.getElementById("sortid").value === "Name"){
 		shows.sort((a, b) => (a.en_name > b.en_name ? 1 : -1));
 		createShows();
+		$("select#sortid").val("Name");
 	} else if (document.getElementById("sortid").value === "Countdown"){
 		shows.sort((a, b) => (a.distance > b.distance ? 1 : -1));
 		createShows();
+		$("select#sortid").val("Countdown");
 	}
 }
 
 /*<select class=\"form-control input-group\" id=\"sortid\" onChange=\"sortBy()\"> <option>Name</option> <option>Countdown</option>*/
 
 function createShows(){
-	document.getElementById("animelist").innerHTML = "<h3 class=\"placeholderText\">Summer 2020</h3><select class=\"form-control input-group\" id=\"sortid\" onChange=\"sortBy()\"> <option>Sort by:</option><option>Name</option> <option>Countdown</option>";
+	document.getElementById("animelist").innerHTML = "<h3 class=\"placeholderText\">Summer 2020</h3><select class=\"form-control input-group\" id=\"sortid\" onChange=\"sortBy()\"> <option disabled>Sort by:</option><option value=\"Name\">Name</option> <option value=\"Countdown\">Countdown</option>";
 
 	for (let i = 0; i < shows.length; i++){
 		document.getElementById("animelist").innerHTML += "<div class=\"container py-3\"><div class=\"card\"><div class=\"row \"><div class=\"col-md-4\"><img src=\"" + shows[i]["img_link"] + "\" height=\"450px\" class=\"w-100\"></div><div class=\"col-md-8 px-3\"><div class=\"card-block px-3\"><h4 class=\"card-title\">" + shows[i]["en_name"] + "</h4><h5 class=\"card-title alttitle\">" + shows[i]["romaji"] + "</h5><h5 class=\"card-title alttitle\">" + shows[i]["jp_name"] + "</h5><p class=\"card-text description\">" + shows[i]["desc"] + "</p><h5>Episodes: " + shows[i]["episodes"] + "</h5><h5 id=\"show" + i + "\"></h5></div></div></div></div></div>";
