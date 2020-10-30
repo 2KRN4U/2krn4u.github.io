3cf95e92301e6ebd36996cd438b182d582d2712f
2KRN4U <2krn4u@gmail.com>
2020-10-30 14:55:28 -0700

    Update nav.js

--- nav.js
+++ nav.js
@@ -2,7 +2,7 @@
 
 let navBar;
 
-const navStart = "<a class=\"navbar-brand\" href=\"#\">2KRN</a> <button class=\"navbar-toggler\" type=\"button\" data-toggle=\"collapse\" data-target=\"#navbarNavAltMarkup\" aria-controls=\"navbarNavAltMarkup\" aria-expanded=\"false\" aria-label=\"Toggle navigation\"> <span class=\"navbar-toggler-icon\"></span> </button> <div class=\"collapse navbar-collapse\" id=\"navbarNavAltMarkup\"> <div class=\"navbar-nav\"> ";
+const navStart = "<a class=\"navbar-brand\" href=\"/\">2KRN</a> <button class=\"navbar-toggler\" type=\"button\" data-toggle=\"collapse\" data-target=\"#navbarNavAltMarkup\" aria-controls=\"navbarNavAltMarkup\" aria-expanded=\"false\" aria-label=\"Toggle navigation\"> <span class=\"navbar-toggler-icon\"></span> </button> <div class=\"collapse navbar-collapse\" id=\"navbarNavAltMarkup\"> <div class=\"navbar-nav\"> ";
 
 let navSearch = "<a class=\"nav-item nav-link\" href=\"../search\">Search</a> ";
 
