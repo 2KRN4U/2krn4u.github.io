fcce920aaeaee6de866624d8b7d299c670483639
2KRN4U <2krn4u@gmail.com>
2020-10-31 18:00:36 -0700

    For Bryan

---
+++ forbryan/main.js
@@ -0,0 +1,28 @@
+// JavaScript Document
+let result = "";
+
+function startConvert(){
+
+	let a = document.getElementById('text').value
+	let b = document.getElementById('text2').value
+	a = parseFloat(a, 10);
+	b = parseFloat(b, 10);
+
+	result = a * 100 / b;
+
+	document.getElementById('results').innerHTML = result;
+}
+
+function convert(event){
+	if (event.keyCode == 13 || event.which == 13){
+		let a = document.getElementById('text').value
+		let b = document.getElementById('text2').value
+		a = parseFloat(a, 10);
+		b = parseFloat(b, 10);
+
+		result = a * 100 / b;
+
+		document.getElementById('results').innerHTML = result;
+	}
+
+}
